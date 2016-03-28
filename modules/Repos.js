6478c4e6c34bd4aae9b9e0d51e364872a6c75f0c
Vargentum <vargentum@gmail.com>
2016-03-28 22:52:55 +0300

    12. programmatically route

--- modules/Repos.js
+++ modules/Repos.js
@@ -1,8 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router'
+import {browserHistory, Link} from 'react-router'
 import NavLink from './NavLink'
 
 export default React.createClass({
+
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const user = ev.target.elements[0].value
+    const repo = ev.target.elements[1].value
+    const path = `/repo/${user}/${repo}`
+
+    // browserHistory.push(path)  //way 1
+    this.context.router.push(path)  //way 2
+  },
+
   render() {
     return (
       <div>
@@ -14,6 +29,13 @@ export default React.createClass({
         <ul>
           <li><Link to="/repo/Foo/Test">Test by Foo</Link></li>
           <li><Link to="/repo/Boo/Rest">Rest by Boo</Link></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
 
         {this.props.children}
